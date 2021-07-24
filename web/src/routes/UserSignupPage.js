999cd8dfc77f58c5e0de0411333ee59a7f80aedf
maxohsawa <max.ohsawa@gmail.com>
2021-07-24 19:26:20 -0400

    adds state for input fields and adds object submission on click

--- web/src/routes/UserSignupPage.js
+++ web/src/routes/UserSignupPage.js
@@ -1,43 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import PageLayout from "parts/PageLayout";
 import { Box, Container, Checkbox, Heading, SimpleGrid, Input, Text, Button } from "@chakra-ui/react";
 
 const UserSignupPage = () => {
+
+    const [formData, setFormData] = useState({});
+
     const history = useHistory();
+
+    const handleChange = (event) => {
+        
+        const id = event.target.id;
+        let value;
+        event.target.type === 'checkbox' ? value = event.target.checked : value = event.target.value;
+        
+        setFormData({...formData, [id]: value});
+    }
+
+    useEffect(() => {
+        console.log(formData);
+    }, [formData])
 
     const handleSkipClick= () => {
         history.push("/workbooks");
     };
 
     const handleSubmit = () => {
-        alert('hi!')
+        alert(JSON.stringify(formData));
     };
 
     return (
         <PageLayout navMargin={false}>
-            <Container maxW="3xl" centerContent>
-                <Heading textTransform="uppercase" as="h2" size="2xl">Welcome!</Heading>
-                <Text>We'd love to know a bit about you.</Text>
+            <Container boxShadow="dark-lg" background="white" padding="50px" maxW="3xl" centerContent>
+                <Heading paddingBottom="40px" textTransform="uppercase" as="h2" size="2xl">Welcome!</Heading>
+                <Text paddingBottom="25px">We'd love to know a bit about you.</Text>
                 <SimpleGrid columns={2} spacingX="40px" spacingY="20px">
+
                     <Box align="right" paddingTop="8px">Name</Box>
-                    <Input variant="flushed"/>
+                    <Input id="name" variant="flushed" onChange={handleChange}/>
+
                     <Box align="right" paddingTop="8px">Organization</Box>
-                    <Input variant="flushed"/>
+                    <Input id="organization" variant="flushed" onChange={handleChange}/>
+
                     <Box align="right" paddingTop="8px">Title</Box>
-                    <Input variant="flushed" />
+                    <Input id="title" variant="flushed" onChange={handleChange}/>
+
                     <Box align="right" paddingTop="8px">Role</Box>
-                    <Input variant="flushed" />
+                    <Input id="role" variant="flushed" onChange={handleChange}/>
+
                     <Box align="right" paddingTop="8px">What space are you working in?</Box>
                     <SimpleGrid columns={2} spacingX="40px" spacingY="10px">
-                        <Checkbox>Researcher</Checkbox>
-                        <Checkbox>Investor</Checkbox>
-                        <Checkbox>Policymaker</Checkbox>
-                        <Checkbox>Other</Checkbox>
-                        <Checkbox>Student</Checkbox>
+
+                        <Checkbox id="cb-researcher" onChange={handleChange}>Researcher</Checkbox>
+
+                        <Checkbox id="cb-investor" onChange={handleChange}>Investor</Checkbox>
+
+                        <Checkbox id="cb-policymaker" onChange={handleChange}>Policymaker</Checkbox>
+
+                        <Checkbox id="cb-other" onChange={handleChange}>Other</Checkbox>
+
+                        <Checkbox id="cb-student" onChange={handleChange}>Student</Checkbox>
+
                     </SimpleGrid>
                     <Box align="right" paddingTop="8px">
-                        <Checkbox></Checkbox>
+                        <Checkbox id="cb-accept-terms" onChange={handleChange}></Checkbox>
                     </Box>
                     <Box paddingTop="3px">
                         I accept the terms and conditions as detailed in the ...
