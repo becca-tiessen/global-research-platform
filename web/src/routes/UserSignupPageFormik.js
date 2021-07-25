6bdfa1dc00331567b239daed0b166dbb67176305
maxohsawa <max.ohsawa@gmail.com>
2021-07-25 19:40:59 -0400

    adds styles, adds requirement function, fixes validate condition

--- web/src/routes/UserSignupPageFormik.js
+++ web/src/routes/UserSignupPageFormik.js
@@ -16,6 +16,7 @@ import {
 const UserSignupPage = () => {
     const { user, patchUserFromAPI } = useContext(UserContext); 
     const [formData, setFormData] = useState({});
+    const [userAgreed, setUserAgreed] = useState(false);
 
     const history = useHistory();
 
@@ -61,7 +62,7 @@ const UserSignupPage = () => {
     };
 
     const validateUserAgreement = (value) => {
-        console.log(value);
+        setUserAgreed(!value);
         let error;
       
         if (!value) {
@@ -90,7 +91,7 @@ const UserSignupPage = () => {
                                     
                                     <Input {...field} id="name" placeholder="" variant="flushed" focusBorderColor="#00477D"/>
                                     
-                                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                                    {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
                                 </FormControl>
                                 )}
                             </Field>
@@ -103,7 +104,7 @@ const UserSignupPage = () => {
                                     
                                 <Input {...field} id="company" placeholder="" variant="flushed" focusBorderColor="#00477D"/>
                                     
-                                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                                    {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
                                 </FormControl>
                                 )}
                             </Field>
@@ -129,50 +130,50 @@ const UserSignupPage = () => {
                                     
                                     <Input {...field} id="location" placeholder="" variant="flushed" focusBorderColor="#00477D"/>
                                     
-                                    <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                                    {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
                                 </FormControl>
                                 )}
                             </Field>
                         </SimpleGrid>
-                        <SimpleGrid columns={2} spacingX="40px" spacingY="20px">
+                        <SimpleGrid columns={2} spacingX="40px" spacingY="20px" paddingTop="10px">
                             <Box align="right" paddingTop="8px">What space are you working in?</Box>
-                            <SimpleGrid columns={2} spacingX="40px" spacingY="10px">
+                            <SimpleGrid columns={2} spacingX="40px" spacingY="10px" paddingTop="10px">
 
-                                    <label>
-                                        Researcher
-                                        <Field type="checkbox" id="cb-researcher" name="cb-researcher" value="researcher"/>
-                                    </label>
+                                    <Box display="flex">
+                                        <label>Researcher</label>
+                                        <Box marginLeft="10px"><Field type="checkbox" id="cb-researcher" name="cb-researcher" value="researcher" /></Box>
+                                    </Box>
+                                    
+                                    <Box display="flex">
+                                        <label>Investor</label>
+                                        <Box marginLeft="10px"><Field type="checkbox" id="cb-investor" name="cb-investor" value="investor"/></Box>
+                                    </Box>
 
-                                    <label>
-                                        Investor
-                                        <Field type="checkbox" id="cb-investor" name="cb-investor" value="investor"/>
-                                    </label>
+                                    <Box display="flex">
+                                        <label>Policymaker</label>
+                                        <Box marginLeft="10px"><Field type="checkbox" id="cb-policymaker" name="cb-policymaker" value="policymaker"/></Box>
+                                    </Box>
 
-                                    <label>
-                                        Policymaker
-                                        <Field type="checkbox" id="cb-policymaker" name="cb-policymaker" value="policymaker"/>
-                                    </label>
+                                    <Box display="flex">
+                                        <label>Other</label>
+                                        <Box marginLeft="10px"><Field type="checkbox" id="cb-other" name="cb-other" value="other"/></Box>
+                                    </Box>
 
-                                    <label>
-                                        Other
-                                        <Field type="checkbox" id="cb-other" name="cb-other" value="other"/>
-                                    </label>
-
-                                    <label>
-                                        Student
-                                        <Field type="checkbox" id="cb-student" name="cb-student" value="student"/>
-                                    </label>
+                                    <Box display="flex">
+                                        <label>Student</label>
+                                        <Box marginLeft="10px"><Field type="checkbox" id="cb-student" name="cb-student" value="student"/></Box>
+                                    </Box>
 
                                 </SimpleGrid>
                                 
                                 <Box align="right" paddingTop="8px">
                                     <Field>
-                                        {({ field, form }) => (
+                                        {({ field, form, touched }) => (
                                             <FormControl isInvalid={form.errors.userAgreement }>
                                                 
-                                                <Field type="checkbox" id="cb-user-agreement" name="userAgreement"  validate={validateUserAgreement}/>
+                                                <Field type="checkbox" id="cb-user-agreement" name="userAgreement" validate={validateUserAgreement}/>
                                                 
-                                                <FormErrorMessage>{form.errors.userAgreement}</FormErrorMessage>
+                                                <FormErrorMessage>{form.touched.userAgreement && form.errors.userAgreement}</FormErrorMessage>
                                             </FormControl>
                                         )}
                                     </Field>
@@ -197,8 +198,10 @@ const UserSignupPage = () => {
                                         background="#00477D" 
                                         padding="25px 40px" 
                                         marginLeft="20px"
-
-                                        
+                                        disabled={userAgreed}
+                                        _hover={{
+                                            boxShadow: "10px 10px 15px rgba(0, 0, 0, 0.2)"
+                                        }}
                                     >
                                     Finish account setup
                                     </Button>
